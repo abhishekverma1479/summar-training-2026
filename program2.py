d3935f9043990c9dddfb609a768e74beab535117
abhishekverma1479 <abhishekverma1479@gmail.com>
2026-07-18 11:01:29 +0530

    program2

---
+++ program2.py
@@ -0,0 +1,17 @@
+name=input("Enter the name:")
+empid=int(input("Employee id:"))
+empdesig=("Employee Designation:")
+empdoj=("Employee Date of joining: ")
+empsal=float(input("Enter the salary: "))
+
+print("#"*40)
+print("Employee Profile")
+print("#"*40)  
+print("Employee ID", empid)
+print("Employee Name", name)
+print("Employee Designation", empdesig)
+print("Employee Date of joining", empdoj)
+print("Enter the salary", empsal)
+          
+          
+                
